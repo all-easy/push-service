14588954dedf76613945f4fa3b3801341a795ef3
Oleg Kucherenko <>
2023-04-21 19:47:55 +0300

    Add percentage

--- src/main/java/ru/all_easy/push/telegram/commands/rules/PushGroupCommandRule.java
+++ src/main/java/ru/all_easy/push/telegram/commands/rules/PushGroupCommandRule.java
@@ -1,6 +1,7 @@
 package ru.all_easy.push.telegram.commands.rules;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
@@ -93,7 +94,33 @@ public class PushGroupCommandRule implements CommandRule {
         
         try {
             BigDecimal calculatedAmount = mathHelper.calculate(messageParts[2]);
-            String name = messageParts.length == 4 ? messageParts[3] : StringUtils.EMPTY;
+            //String name = messageParts.length == 4 ? messageParts[3] : StringUtils.EMPTY;
+            String name = StringUtils.EMPTY;
+
+            // Case [0]/push [1]@username [2]math_expr [3]expNameOrPercentage?
+            if (messageParts.length == 4) {
+                Optional<Integer> percentageNumberOptional = validatePercentage(messageParts[3]);
+                if (percentageNumberOptional.isPresent()) {
+                    int percentageNumber = percentageNumberOptional.get();
+                    calculatedAmount = BigDecimal.valueOf(calculatedAmount.doubleValue() * (100 + percentageNumber) / 100);
+                    name = StringUtils.EMPTY;
+                } else {
+                    name = messageParts[3];
+                }
+            }
+
+            // Case [0]/push [1]@username [2]math_expr [3]expName [4]Percentage
+            if (messageParts.length == 5) {
+                name = messageParts[3];
+                Optional<Integer> percentageNumberOptional = validatePercentage(messageParts[4]);
+                if (percentageNumberOptional.isPresent()) {
+                    int percentageNumber = percentageNumberOptional.get();
+                    calculatedAmount = BigDecimal.valueOf(calculatedAmount.doubleValue() * (100 + percentageNumber) / 100);
+                } else {
+                    throw new IllegalArgumentException();
+                }
+            }
+
             ExpenseInfo info = new ExpenseInfo(
                 roomEntity.getToken(), 
                 fromEntity.getUserUid(), 
@@ -121,6 +148,20 @@ public class PushGroupCommandRule implements CommandRule {
         }
 
         return true;
+    }
+
+    private Optional<Integer> validatePercentage(String value) {
+        if (!value.contains("%") || value.isBlank() || value.equals(" ")) return Optional.empty();
+
+        String valueStr = value.replace("%", "");
+        for (int i = 0; i < valueStr.length(); i++) {
+            if (!Character.isDigit(valueStr.charAt(i))) return Optional.empty();
+        }
+
+        int valueInt = Integer.parseInt(valueStr);
+        if (valueInt <= 0 || valueInt > 100) return Optional.empty();
+
+        return Optional.of(valueInt);
     }
 
     private RoomUserEntity findRoomUser(RoomEntity room, String username) {
