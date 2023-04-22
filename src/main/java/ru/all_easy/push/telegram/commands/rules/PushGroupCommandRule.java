b9412383da95f6961d7c645cb8f5647c54864c54
Oleg Kucherenko <>
2023-04-22 16:50:36 +0300

    Rollback changes with percentage

--- src/main/java/ru/all_easy/push/telegram/commands/rules/PushGroupCommandRule.java
+++ src/main/java/ru/all_easy/push/telegram/commands/rules/PushGroupCommandRule.java
@@ -94,33 +94,7 @@ public class PushGroupCommandRule implements CommandRule {
         
         try {
             BigDecimal calculatedAmount = mathHelper.calculate(messageParts[2]);
-            //String name = messageParts.length == 4 ? messageParts[3] : StringUtils.EMPTY;
-            String name = StringUtils.EMPTY;
-
-            // Case [0]/push [1]@username [2]math_expr [3]expNameOrPercentage?
-            if (messageParts.length == 4) {
-                Optional<Integer> percentageNumberOptional = validatePercentage(messageParts[3]);
-                if (percentageNumberOptional.isPresent()) {
-                    int percentageNumber = percentageNumberOptional.get();
-                    calculatedAmount = BigDecimal.valueOf(calculatedAmount.doubleValue() * (100 + percentageNumber) / 100);
-                    name = StringUtils.EMPTY;
-                } else {
-                    name = messageParts[3];
-                }
-            }
-
-            // Case [0]/push [1]@username [2]math_expr [3]expName [4]Percentage
-            if (messageParts.length == 5) {
-                name = messageParts[3];
-                Optional<Integer> percentageNumberOptional = validatePercentage(messageParts[4]);
-                if (percentageNumberOptional.isPresent()) {
-                    int percentageNumber = percentageNumberOptional.get();
-                    calculatedAmount = BigDecimal.valueOf(calculatedAmount.doubleValue() * (100 + percentageNumber) / 100);
-                } else {
-                    throw new IllegalArgumentException();
-                }
-            }
-
+            String name = messageParts.length == 4 ? messageParts[3] : StringUtils.EMPTY;
             ExpenseInfo info = new ExpenseInfo(
                 roomEntity.getToken(), 
                 fromEntity.getUserUid(), 
@@ -148,20 +122,6 @@ public class PushGroupCommandRule implements CommandRule {
         }
 
         return true;
-    }
-
-    private Optional<Integer> validatePercentage(String value) {
-        if (!value.contains("%") || value.isBlank() || value.equals(" ")) return Optional.empty();
-
-        String valueStr = value.replace("%", "");
-        for (int i = 0; i < valueStr.length(); i++) {
-            if (!Character.isDigit(valueStr.charAt(i))) return Optional.empty();
-        }
-
-        int valueInt = Integer.parseInt(valueStr);
-        if (valueInt <= 0 || valueInt > 100) return Optional.empty();
-
-        return Optional.of(valueInt);
     }
 
     private RoomUserEntity findRoomUser(RoomEntity room, String username) {
