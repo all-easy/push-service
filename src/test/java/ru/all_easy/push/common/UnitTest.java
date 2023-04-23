a5eb5e1dd6b57af99d075350d075a30dcdd0cfd9
Denis Kartavenko <denis.kartavenko@dif.tech>
2023-04-23 17:54:40 +0400

    add tests

---
+++ src/test/java/ru/all_easy/push/common/UnitTest.java
@@ -0,0 +1,8 @@
+package ru.all_easy.push.common;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public abstract class UnitTest {
+}
