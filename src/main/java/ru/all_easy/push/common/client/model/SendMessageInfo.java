2f3be5c9029ce638f2eac6e141387201a93581fd
Oleg Kucherenko <>
2023-04-28 20:05:08 +0300

    Implement serializable

--- src/main/java/ru/all_easy/push/common/client/model/SendMessageInfo.java
+++ src/main/java/ru/all_easy/push/common/client/model/SendMessageInfo.java
@@ -1,8 +1,10 @@
 package ru.all_easy.push.common.client.model;
 
+import java.io.Serializable;
+
 public record SendMessageInfo(
     Long chatId,
     String text,
     String parseMode
-) {
+) implements Serializable {
 }
