86ff3be4e928fce8cf2a8950ae321f5481432ffc
Denis-26 <kartavenkodenis0@gmail.com>
2023-04-30 15:06:42 +0300

    add block to pushing on yourself (#10)
    Co-authored-by: Denis Kartavenko <denis.kartavenko@dif.tech>

--- src/main/java/ru/all_easy/push/telegram/messages/AnswerMessageTemplate.java
+++ src/main/java/ru/all_easy/push/telegram/messages/AnswerMessageTemplate.java
@@ -4,7 +4,8 @@ public enum AnswerMessageTemplate {
 
     UNREGISTERED_ROOM("It seems virtual room is empty, send /addme command and ask others to do same 🙃"),
     UNREGISTERED_USER("I guess user: *%s* is not registered, ask him to call /start command in private messages with me please"),
-    UNADDED_USER("I guess user: *%s* is not in virtual group, ask him to call /addme command");
+    UNADDED_USER("I guess user: *%s* is not in virtual group, ask him to call /addme command"),
+    YOURSELF_PUSH("You can't push to yourself \uD83D\uDE22");
 
     private final String message;
 
