89d870599fc6cddb75c6d373ba37ce199de89304
Oleg Kucherenko <>
2023-05-03 16:06:18 +0300

    Refactor ResultGroupCommandRule

--- src/main/java/ru/all_easy/push/telegram/commands/service/ResultGroupCommandService.java
+++
@@ -1,8 +0,0 @@
-package ru.all_easy.push.telegram.commands.service;
-
-import ru.all_easy.push.common.client.model.SendMessageInfo;
-import ru.all_easy.push.telegram.api.controller.model.Update;
-
-public interface ResultGroupCommandService {
-    SendMessageInfo getResult(Update update);
-}
