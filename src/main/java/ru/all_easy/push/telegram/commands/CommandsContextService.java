a8246ae92803165c1fd7ebb7670c325faa1d3e27
Denis Kartavenko <denis.kartavenko@dif.tech>
2023-05-14 00:05:29 +0400

    refactor send message flow

--- src/main/java/ru/all_easy/push/telegram/commands/CommandsContextService.java
+++ src/main/java/ru/all_easy/push/telegram/commands/CommandsContextService.java
@@ -1,17 +1,13 @@
 package ru.all_easy.push.telegram.commands;
 
 import org.springframework.stereotype.Service;
-import ru.all_easy.push.common.ResultK;
 import ru.all_easy.push.common.client.model.SendMessageInfo;
 import ru.all_easy.push.telegram.api.ParseMode;
 import ru.all_easy.push.telegram.api.controller.model.Update;
 import ru.all_easy.push.telegram.api.service.TelegramService;
 import ru.all_easy.push.telegram.commands.rules.CommandRule;
-import ru.all_easy.push.telegram.commands.rules.model.CommandError;
-import ru.all_easy.push.telegram.commands.rules.model.CommandProcessed;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CommandsContextService {
@@ -30,18 +26,18 @@ public class CommandsContextService {
             return;
         }
 
-        Optional<CommandRule> rule = commands.stream()
-                .filter(it -> it.apply(update))
-                .findFirst();
-        if (rule.isPresent()) {
-            var chatId = update.message().chat().id();
-            ResultK<CommandProcessed, CommandError> processed = rule.get().process(update);
-            if (processed.hasError()) {
-                sendMessage(new SendMessageInfo(chatId, processed.getError().message(), ParseMode.MARKDOWN.getMode()));
-            } else {
-                sendMessage(new SendMessageInfo(chatId, processed.getResult().message(), ParseMode.MARKDOWN.getMode()));
-            }
-        }
+        commands.stream()
+            .filter(it -> it.apply(update))
+            .findFirst()
+            .map(commandRule -> commandRule.process(update))
+            .ifPresent(result -> {
+                var chatId = update.message().chat().id();
+                if (result.hasError()) {
+                    sendMessage(new SendMessageInfo(chatId, result.getError().message(), ParseMode.MARKDOWN.getMode()));
+                } else {
+                    sendMessage(new SendMessageInfo(chatId, result.getResult().message(), ParseMode.MARKDOWN.getMode()));
+                }
+            });
     }
 
     private void sendMessage(SendMessageInfo message) {
