7b8c8d14f766a6406d13a24409a4a7b666444ff0
OlegKDev <OlegKDeveloper@gmail.com>
2023-06-09 17:29:41 +0300

    Remove all xml files, migrate to yaml properties (#17)

--- src/main/java/ru/all_easy/push/telegram/api/service/TelegramConfig.java
+++ src/main/java/ru/all_easy/push/telegram/api/service/TelegramConfig.java
@@ -2,6 +2,6 @@ package ru.all_easy.push.telegram.api.service;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "telegram")
+@ConfigurationProperties(prefix = "telegram.hook")
 public record TelegramConfig(String hook, boolean dropPendingUpdates) {
 }
