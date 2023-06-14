601c5a253b0f961eacc96e8f9c1a7f605d9d9d16
Denis Kartavenko <denis.kartavenko@dif.tech>
2023-06-14 13:30:58 +0400

    returned record

--- src/main/java/ru/all_easy/push/currency/service/model/CurrencyInfo.java
+++ src/main/java/ru/all_easy/push/currency/service/model/CurrencyInfo.java
@@ -1,19 +1,3 @@
 package ru.all_easy.push.currency.service.model;
 
-public class CurrencyInfo {
-    private final String code;
-    private final String symbol;
-
-    public CurrencyInfo(String code, String symbol) {
-        this.code = code;
-        this.symbol = symbol;
-    }
-
-    public String code() {
-        return code;
-    }
-
-    public String symbol() {
-        return symbol;
-    }
-}
+public record CurrencyInfo(String code, String symbol) {}
