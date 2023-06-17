172713d1190f50b651cdf72f473b69b9e1c602be
OlegKDev <OlegKDeveloper@gmail.com>
2023-06-17 15:32:29 +0300

    Revert "Auto chat migration (#37)"
    This reverts commit 62820b815af33a9fb4ca817176a1d0126d568ae0.

--- src/main/java/ru/all_easy/push/telegram/api/controller/UpdateController.java
+++ src/main/java/ru/all_easy/push/telegram/api/controller/UpdateController.java
@@ -2,18 +2,17 @@ package ru.all_easy.push.telegram.api.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
 import ru.all_easy.push.common.AbstractAuthentication;
 import ru.all_easy.push.telegram.api.controller.model.Update;
-import ru.all_easy.push.telegram.api.supergroup.GroupToSuperGroupEvent;
 import ru.all_easy.push.telegram.commands.CommandsContextService;
 
 @RestController
 @RequestMapping("/v1/api/telegram/${telegram.hook.secret}")
 public class UpdateController extends AbstractAuthentication {
+
     private static final Logger logger = LoggerFactory.getLogger(UpdateController.class);
 
     @ExceptionHandler
@@ -24,20 +23,13 @@ public class UpdateController extends AbstractAuthentication {
 
     private final CommandsContextService commandContextService;
 
-    private ApplicationEventPublisher applicationEventPublisher;
-
-    public UpdateController(
-            ApplicationEventPublisher applicationEventPublisher, CommandsContextService commandContextService) {
-        this.applicationEventPublisher = applicationEventPublisher;
+    public UpdateController(CommandsContextService commandContextService) {
         this.commandContextService = commandContextService;
     }
 
     @PostMapping("/")
     public void postMethodName(@RequestBody Update update) {
         logger.info("Update received: {}", update);
-        if (update.message().migrateFromChatId() != null && update.message().migrateToChatId() != null) {
-            GroupToSuperGroupEvent event = new GroupToSuperGroupEvent(this, update);
-            applicationEventPublisher.publishEvent(event);
-        } else commandContextService.process(update);
+        commandContextService.process(update);
     }
 }
